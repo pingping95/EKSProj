cdbde850d9443891d45a4742368dc31860e9ee4b
pingping95 <xogns556@naver.com>
2021-11-24 22:10:00 +0900

    edit some codes

--- ec2.tf
+++ ec2.tf
@@ -19,20 +19,18 @@
 
 
 // Jenkins EC2
-# module "jenkins" {
-#   source         = "../terraform/modules/ec2"
-#   name           = "${local.name_prefix}-public-server"
-#   instance_count = 1
-
-#   ami                    = data.aws_ami.amazon2.id
-#   instance_type          = var.jenkins.instance_type
-#   key_name               = var.key_pair
-#   vpc_security_group_ids = [aws_security_group.jenkins.id, aws_security_group.bastion.id]
-#   subnet_ids             = module.main_vpc.public_subnets_ids // Network Configuration
-#   user_data              = base64encode(file("./scripts/common/install_jenkins.sh"))
-
-#   tags = {
-#     Environment = var.tags.Environment
-#     Owner       = var.owner_tag
-#   }
-# }
+module "jenkins" {
+  source         = "../terraform/modules/ec2"
+  name           = "${local.name_prefix}-public-server"
+  instance_count = 1
+  ami                    = data.aws_ami.amazon2.id
+  instance_type          = var.jenkins.instance_type
+  key_name               = var.key_pair
+  vpc_security_group_ids = [aws_security_group.jenkins.id, aws_security_group.bastion.id]
+  subnet_ids             = module.main_vpc.public_subnets_ids // Network Configuration
+  user_data              = base64encode(file("./scripts/common/install_jenkins.sh"))
+  tags = {
+    Environment = var.tags.Environment
+    Owner       = var.owner_tag
+  }
+}
